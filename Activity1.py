fddece590ef704f4813a32c7cbeb0fa9f16a9fab
shreyanshiBarat <shreyanshi6972@ggn.hxls.org>
2026-08-07 19:45:32 +0530

    Python Test-3

---
+++ Activity1.py
@@ -0,0 +1,16 @@
+name_score=
+
+"student1"={"lily":"10"},
+"student2"={"daniela":"25"},
+"student3"={"lisa":"20"},
+"student4"={"ava":"50"},
+"student5"={"lara":"30"}
+average= 60**5
+for i in average:
+   print(average,"is the average of the class")
+maximum= 40<50
+minimum=0<19
+if max(40<50):
+   print(maximum,"is the most top score of the class")
+if min(0<19):
+   print(minimum,"is the lowest score of the class")
